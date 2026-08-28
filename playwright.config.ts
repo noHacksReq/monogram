72360e2d5f2728d9504b1c6eb7799985c93909b2
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2026-08-28 15:03:19 -0400

    moved use object

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,7 +74,9 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:3000',
-    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
+  use: {
+    baseURL: 'http://localhost:3000',
+  }
 });
