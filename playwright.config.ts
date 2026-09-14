2f6bcd2268b0f7d9211ba78d57f859bdc5061fba
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2026-09-14 15:51:28 -0400

    updated pw config

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev --prefix app',
+    command: 'npm run dev',
+    cwd: './app',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     stdout: 'ignore',
