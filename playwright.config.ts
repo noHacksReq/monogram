a7504909d8f1217de21a37276ff00126dee379c0
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2026-09-14 15:38:55 -0400

    updated command in webserver to point at app folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,11 +72,11 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run dev --prefix app',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     stdout: 'ignore',
     stderr: 'pipe',
-    cwd: '/app'
+   
   },
 });
