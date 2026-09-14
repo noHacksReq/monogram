ee1cee88b62930760df23e1ad649437837a74da4
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2026-09-14 15:27:09 -0400

    updated command in webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'vite',
+    command: 'npm run build && npm run preview',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     stdout: 'ignore',
